94701fbc379924d464bdfa6ab11fae53630fae77
Courdevelops <courdevelops@gmail.com>
2024-10-31 16:28:50 -0400

    some pin data

--- login_pin_field.js
+++ login_pin_field.js
@@ -5,58 +5,44 @@
 // FIRST query the Inputs
 const inputs = document.querySelectorAll('input');
 
-inputs.forEach((input, key) => {
-    // Test print
-    console.log(inputs);
-    // 
-    if (key != 0) {
-        input.addEventListener('click', function () {
-            inputs[0].focus();
-        });
-    }
-    // Add event listener function to input that iterates through the inputs
-    input.addEventListener('keyup', function () {
-        if (input.value) {
-            // Then Do the stuff
-            console.log("Num or Let entered");
-            if (key === 4) {
-                // Map through the inputs
-                const pinCode = [...inputs].map((input) => input.value).join("");
-            }
-            // Match pincode to registered (for login)
-            
-        }
-    });
-});
-
-// const inputs = document.querySelectorAll("input");
+// query the pin puts to record the number
+const whole_pin = document.getElementById('pin_field');
 // const codeBlock = document.getElementById("code-block");
 // const code = document.getElementById("code");
 // const form = document.querySelector("form");
 
-// inputs.forEach((input, key) => {
+inputs.forEach((input, key) => {
     // If key is not 0
-//   if (key !== 0) {
+  if (key !== 0) {
         // Event listener for focus when input focused on (for skip ahead)
-//     input.addEventListener("click", function () {
-//       inputs[0].focus();
-//     });
-//   }
+    input.addEventListener("click", function () {
+      inputs[0].focus();
+    });
+  }
     // Add event listener function to input that iterates through the inputs
-//   input.addEventListener("keyup", function () {
-//     if (input.value) {
-//       if (key === 3) {
-//         // Last one tadaa
-//         const userCode = [...inputs].map((input) => input.value).join("");
-            // This is for the revelation on the page of your Pin #
-//         codeBlock.classList.remove("hidden");
-//         code.innerText = userCode;
-//       } else {
-//         inputs[key + 1].focus();
-//       }
-//     }
-//   });
-// });
+  input.addEventListener("keyup", function () {
+    if (input.value) {
+        console.log("Pin Num entered")
+        // BELOW is saying if key is less than 4
+            // because 0 is starting point
+                // jump focus ahead
+      if (key === 3) {
+    //     // Last one tadaa
+    // const userCode = [...inputs].map((input) => input.value).join("");
+    //     // This is for the revelation on the page of your Pin #
+    //     // codeBlock.classList.remove("hidden");
+    //     // code.innerText = userCode;
+      } else {
+        inputs[key + 1].focus();
+      }
+      const userCode = [...inputs].map((input) => input.value).join("");
+    // BELOW grabs each digit as you type
+      console.log(userCode);
+    }
+    
+  });
+  
+});
 
 // const reset = () => {
 //   form.reset();
