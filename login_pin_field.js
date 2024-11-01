7b85c254597c650c4e3b2aa69f449cd9cf29d1fa
Courdevelops <courdevelops@gmail.com>
2024-11-01 16:46:40 -0400

    clear button works

--- login_pin_field.js
+++ login_pin_field.js
@@ -1,7 +1,16 @@
+// WHOLE FORM
+
+// FOR A CLEAR BUTTON TO RESET THE PAGE
+
+// query the whole form
+const form = document.querySelector('form');
+
+// query the clear button
+const clear_button = document.getElementById('reset_button');
+
 // LOG IN NAME
 
 // FIRST query the name selected
-
 
 // PIN FIELDS
 
@@ -12,10 +21,6 @@ const inputs = document.querySelectorAll('input');
 
 // query the pin puts to record the number
 const whole_pin = document.getElementById('pin_field');
-
-// SUBMIT BUTTON
-
-// change the color of the button when all 4 digits entered
 
 
 
@@ -51,6 +56,8 @@ inputs.forEach((input, key) => {
       // If the Pin Num is full, change color of button
       if (key === 3) {
         console.log("All pins full");
+        // SUBMIT BUTTON
+        // change the color of the button when all 4 digits entered
         // query the submit button 
         let submit_button = document.getElementById('submit_login');
         // Change the color of the Submit Button
@@ -62,10 +69,18 @@ inputs.forEach((input, key) => {
   
 });
 
-// const reset = () => {
-//   form.reset();
-//   codeBlock.classList.add("hidden");
-//   code.innerText = "";
-// };
+// Call this function in the Clear button
+const reset = () => {
+  form.reset();
+  // codeBlock.classList.add("hidden");
+  // code.innerText = "";
+};
 
+
+
+// Add event listener to the reset button
+clear_button.addEventListener('click', function () {
+  console.log("Clear Button Clicked");
+  reset();
+})
 
